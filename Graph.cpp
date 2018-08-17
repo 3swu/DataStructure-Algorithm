364787fffaef704ed3e0b838a89d6b813a6d7cb3
wuleiaty <1046828876@qq.com>
2018-08-17 21:24:24 +0800

    update dfs

--- Graph.cpp
+++ Graph.cpp
@@ -4,6 +4,8 @@
 
 void BFSTraverse(MGraph &);
 void BFS(MGraph, bool*, int);
+void DFSTraverse(MGraph &);
+void DFS(MGraph, bool*, int);
 
 void BFSTraverse(MGraph &G){
     bool visited[G.VexNum + 1];
@@ -31,15 +33,40 @@ void BFS(MGraph G, bool* visited, int v){
     }
 }
 
+void DFSTraverse(MGraph &G){
+    bool visited[G.VexNum + 1];
+    for(int i = 1; i <= G.VexNum; i++)
+        visited[i] = false;
+    for(int i = 1; i <= G.VexNum; i++){
+        if(!visited[i])
+            DFS(G, visited, i);
+    }
+}
+
+void DFS(MGraph G, bool* visited, int v){
+    cout << G.Vex[v] << endl;
+    visited[v] = true;
+    queue<int> q;
+    q.push(v);
+    while(!q.empty()){
+        v = q.front();
+        q.pop();
+        for(int w = firstNeighbor(G, v); w >= 0; w = nextNeighbor(G, v, w))
+            if(!visited[w])
+                DFS(G, visited, w);
+    }
+}
+
 int main(){
     MGraph g;
     cout << initGraphByFile(g) << endl;
     BFSTraverse(g);
-    cout << adjacent(g, 4, 5) << endl;
-    neighbors(g, 4);
-    cout << insertVertex(g, 'f') << endl;
-    cout << deleteVertex(g, 'c') << endl;
-    cout << addEdge(g, 5, 6) << endl;
-    cout << removeEdge(g, 4, 5) << endl;
-    cout << firstNeighbor(g, 1) << endl;
+    DFSTraverse(g);
+    // cout << adjacent(g, 4, 5) << endl;
+    // neighbors(g, 4);
+    // cout << insertVertex(g, 'f') << endl;
+    // cout << deleteVertex(g, 'c') << endl;
+    // cout << addEdge(g, 5, 6) << endl;
+    // cout << removeEdge(g, 4, 5) << endl;
+    // cout << firstNeighbor(g, 1) << endl;
 }
